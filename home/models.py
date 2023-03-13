55181212506d0fba532334459de831afacc060af
Richard Allen <richard@enzedonline.com>
2023-03-13 15:42:49 +0100

    testing

--- home/models.py
+++ home/models.py
@@ -3,12 +3,11 @@ from wagtail.models import Page
 import plotly.express as px
 
 class HomePage(Page):
-    template = 'home/home.html'
-    pass
+    template = 'home/home_page.html'
 
-    @property
-    def plot(self):
-        data_canada = px.data.gapminder().query("country == 'Canada'")
-        fig = px.bar(data_canada, x='year', y='pop', title='Test Graph')
-        return fig.to_html(full_html=False)
+    # @property
+    # def plot(self):
+    #     data_canada = px.data.gapminder().query("country == 'Canada'")
+    #     fig = px.bar(data_canada, x='year', y='pop', title='Test Graph')
+    #     return fig.to_html(full_html=False)
 
