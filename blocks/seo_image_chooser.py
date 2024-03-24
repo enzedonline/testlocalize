a7bba43aef10a8dc73fde7a3bfc94a1c85026ace
Richard Allen <richard@enzedonline.com>
2024-03-24 14:40:38 +1300

    seo image chooser updates

--- blocks/seo_image_chooser.py
+++ blocks/seo_image_chooser.py
@@ -1,68 +1,61 @@
-from django import forms
 from django.forms.utils import ErrorList
 from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
-from wagtail.blocks import StructBlock, StructBlockValidationError
-from wagtail.blocks.struct_block import StructBlockAdapter
+from wagtail.blocks import CharBlock, StructBlock
+from wagtail.blocks.struct_block import (StructBlockAdapter,
+                                         StructBlockValidationError)
 from wagtail.telepath import register
 
-from blocks.wagtail.blocks import CharBlock, ImageChooserBlock, RequiredMixin
+from blocks.base_blocks import CustomImageChooserBlock
 
 
-class SEOImageChooserBlock(RequiredMixin, StructBlock):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        if kwargs:
-            self.declared_blocks["image"].required = kwargs.get("required", True)
-            self.declared_blocks["seo_title"].required = kwargs.get("required", True)
+class SEOImageChooserBlock(StructBlock):
+    def __init__(self, required=True, **kwargs):
+        local_blocks = (
+            ("image", CustomImageChooserBlock(
+                label=_("Image"),
+                required=required,
+                form_classname="compact-image-chooser"
+            )),
+            ("description", CharBlock(
+                label=_("Description"),
+                help_text=_(
+                    "A contextual description of the image for screen readers and search engines"
+                ),
+                required=required
+            )),
+        )   
+        super().__init__(local_blocks, **kwargs)
 
-    image = ImageChooserBlock(label=_("Image"))
-    seo_title = CharBlock(
-        label=_("SEO Title"),
-        help_text=_(
-            "A text description of the image for screen readers and search engines"
-        ),
-    )
+    
 
     class Meta:
-        form_classname = "seo-image-chooser-block"
-
-    @cached_property
-    def image_error(self):
-        return ErrorList([_("Please select an image")])
-
-    @cached_property
-    def title_error(self):
-        return ErrorList([_("Please enter a descriptive SEO title for the image")])
+        icon = "image"
+        label_format = '{seo_title}'
+        template = 'blocks/image_block.html'
+        form_classname = "structblock seo-image-chooser-block"
 
     def clean(self, value):
-        errors = {}
-        image = value.get("image")
-        seo_title = value.get("seo_title")
-        if self.required:
-            if not image:
-                errors["image"] = self.image_error
-            if not seo_title:
-                errors["seo_title"] = self.title_error
-        elif image and not seo_title:
-            errors["seo_title"] = self.title_error
-
-        if errors:
-            raise StructBlockValidationError(block_errors=errors)
-
-        return super().clean(value)
-
+        # standard form validation
+        cleaned_data = super().clean(value)
+        # custom validation to run if standard form validation passes
+        if not self.required and (bool(value['image']) and not bool(value['description'])):
+            raise StructBlockValidationError(
+                block_errors={'description': ErrorList(
+                    [_("Please enter a text description for the image.")])}
+            )
+        return cleaned_data
 
 class SEOImageChooserBlockAdapter(StructBlockAdapter):
-    js_constructor = "blocks.models.SEOImageChooserBlock"
+    js_constructor = "blocks.seo_image_chooser.SEOImageChooserBlock"
 
     @cached_property
     def media(self):
+        from django import forms
         structblock_media = super().media
         return forms.Media(
             js=structblock_media._js + ["js/seo-image-chooser-block.js"],
             css={"all": ("css/seo-image-chooser-block.css",)},
         )
-
-
+    
 register(SEOImageChooserBlockAdapter(), SEOImageChooserBlock)
